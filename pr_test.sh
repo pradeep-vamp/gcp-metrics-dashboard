aa5556e50d41ce36bc1d5f7ca06451e0568413db
Pradeep Shankar <pradeep@vamp.me>
2023-02-28 17:50:30 +0530

    DATA-97 - Repo Upgrade

---
+++ pr_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Not working - trialing script
+
+set -ex
+
+PROJECT_ID="$1"
+
+DEBIAN_FRONTEND=noninteractive apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install awscli build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev
+DEBIAN_FRONTEND=noninteractive python -m pip install --upgrade pip
+DEBIAN_FRONTEND=noninteractive pip install pytest
+DEBIAN_FRONTEND=noninteractive pip install flake8
+DEBIAN_FRONTEND=noninteractive pip install awsebcli --upgrade --user
+
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+# exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
